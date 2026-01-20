169f0b0334b3f24bd46623116060b1f0d0f33bc2
Mr.Gr3b3r <roygr277@gmail.com>
2026-01-20 19:55:06 +0200

    since I kinda took a little break from the project I decided to reimplement the canvas component, so currently I got a canvas that can draw but it most be positioned at (0,0) for it to work however no worries I will implement the movement of the canvas the zoom and other features that someone that uses a drawing application would expect from their canvas, I also started considering to make everything a bit more efficient although currently everything runs with 240+ fps and 240 fps is because of my monitor.

---
+++ UI/canvas.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#ifndef CANVAS_H
+#define CANVAS_H
+#include <defines.h>
+#include <utils.h>
+#include <sprite.h>
+#include <camera.h>
+
+typedef struct brush {
+    color_t color;
+    f32 size;
+} brush_t;
+
+typedef struct canvas {
+    comp_header_t header;
+
+    void* parent;
+
+    brush_t brush;
+    struct {
+        i32 x;
+        i32 y;
+    } prev;
+
+    camera_t* camera;
+    sprite_t* sprite;
+    texture_t* tex;
+} canvas_t;
+
+canvas_t* new_canvas(void* parent, const bounding_box* box);
+void del_canvas(canvas_t* canvas);
+void bind_canvas(canvas_t* canvas);
+void set_brush(canvas_t* canvas, const color_t color, const f32 size);
+void update_canvas(canvas_t* canvas, const mat4* projection);
+
+#endif //CANVAS_H
