bcfcb07d6289017611d899232e12029d1bb83e79
MrGreber <roygr277@gmail.com>
2026-05-14 12:48:55 +0300

    Now it forces the window to redraw when restored after minimization

--- UI/frame.c
+++ UI/frame.c
@@ -52,12 +52,11 @@ extern void __keyboard_callback(GLFWwindow* window, i32 key, i32 sc, i32 action,
 extern void __mouse_movement_callback(GLFWwindow* window, const f64 mouse_x, const f64 mouse_y);
 extern void __mouse_button_callback(GLFWwindow* window, const i32 button, const i32 action, const i32 mods);
 extern void __scroll_callback(GLFWwindow* window, const f64 x, const f64 scroll_y);
-// todo: make a necessary callback for minimization/maximization.
-// static void __maximize_callback(GLFWwindow* window, const i32 flag) {
-//     frame_t* frame = glfwGetWindowUserPointer(window);
-//     frame->header.dirty = 1;
-//     printf("hello?\n");
-// }
+static void __restore_callback(GLFWwindow* window, const i32 flag) {
+    if (flag) return;
+    frame_t* frame = glfwGetWindowUserPointer(window);
+    frame->header.dirty = 2;
+}
 
 static void private(tick)(frame_t* frame) {
     _Thread_local static char caption[64] = { 0 };
@@ -100,8 +99,7 @@ frame_t* Frame(new)(const color_t bg, const u32 width, const u32 height, const c
     glfwMakeContextCurrent(frame->ctx);
     glfwSetFramebufferSizeCallback(frame->ctx, __resize_callback);
     // todo: look into this
-    // glfwSetWindowMaximizeCallback(frame->ctx, __maximize_callback);
-    // glfwSetWindowIconifyCallback(frame->ctx, __maximize_callback);
+    glfwSetWindowIconifyCallback(frame->ctx, __restore_callback);
     glfwSetKeyCallback(frame->ctx, __keyboard_callback);
     glfwSetCursorPosCallback(frame->ctx, __mouse_movement_callback);
     glfwSetMouseButtonCallback(frame->ctx, __mouse_button_callback);
